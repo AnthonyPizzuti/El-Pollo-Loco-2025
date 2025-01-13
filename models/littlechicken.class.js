05f1e83621b49161f52a973fabe774253af18a81
Anthony Pizzuti <anthony.pizzuti@gmx.de>
2025-01-13 23:55:16 +0100

    change little enemies width

--- models/littlechicken.class.js
+++ models/littlechicken.class.js
@@ -1,7 +1,7 @@
 class Littlechicken extends MovableObject {
   y = 380;
   height = 50;
-  width = 100;
+  width = 70;
   IMAGES_WALKING = [
     "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
     "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
